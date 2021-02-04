4e47f81d4b91d3b8e4ff7ca8089d0051426caed9
testuser1 <testuser1@ip-10-1-1-55.ap-south-1.compute.internal>
2021-02-04 08:40:19 +0000

    added users in the machine

--- details.sh
+++ details.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "this script will provide information about the local system"
-echo hostame
-echo$[whoami] 
-echo$[id-um]
-echo$[rahul]
+echo
+#GIVES HOSTNAME
+echo  hostame
+echo
+#GIVES USER.EXECUTED THIS SCRIPT
+echo $(whoami) 
+echo
+#GIVES USERS IN THE SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $users
